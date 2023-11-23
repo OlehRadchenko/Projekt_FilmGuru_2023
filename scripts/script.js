2e7efec7c4cec62f882f3afd0c138e1a769e11e5
Ciufciuf <PaluchBartosz@onet.eu>
2023-11-23 21:28:26 +0100

    Display the searched movies and show their images.

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,144 @@
 const url = 'https://api.themoviedb.org/3';
 const api_key = 'api_key=c55b1cd24cb5bed30d81132775cd9903';
 let movie_id;
+let id_backdrops = 0;
+let id_trailers = 0;
 
-const buttonClick = (form) =>{
-    let keyword = form.keyword.value;
-    printAll(url+'/search/movie?query='+keyword+'&'+api_key, printFilms);
-}
+const buttonClick = form => {
+	let keyword = form.keyword.value;
+	printAll(url + '/search/movie?query=' + keyword + '&' + api_key, printFilms);
+};
 
 const printAll = (url, fn) => {
-    fetch(url)
-    .then(checkStatus)
-    .then(fn)
-    .catch(printError)
-}
+	fetch(url).then(checkStatus).then(fn).catch(printError);
+};
 
-const checkStatus = (response) =>{
-    if(response.status == 200){
-        return response.json();
-    }else{
-        throw new Error('Błąd w pobieraniu danych');
-    }
-}
+const checkStatus = response => {
+	if (response.status == 200) {
+		return response.json();
+	} else {
+		throw new Error('Błąd w pobieraniu danych');
+	}
+};
 
-const printData = (json) =>{
-    let title = document.createElement('h1');
-    let poster = document.createElement('img');
-    let overwiev = document.createElement('h2');
-    let release_date = document.createElement('h2');
-    let genre = document.createElement('h2');
-    let themoviedb_link = document.createElement('a');
-    let vote_average = document.createElement('h3');
-    let vote_count = document.createElement('h3');
-    let backdrops = document.createElement('div');
-    let trailer = document.createElement('a');
-    console.log(json);
-    const titles = json.title.toLowerCase().split(' ');
-    const titlee = titles.join("_");
-    console.log(
-        "Title: "+json.title+"\n"+
-        "Poster: href: https://image.tmdb.org/t/p/w500"+json.poster_path+"\n"+
-        "Overwiev: "+json.overview+"\n"+
-        "Release date: "+json.release_date+"\n"+
-        "Genre: "+json.genres+"\n"+
-        "themoviedb link: https://www.themoviedb.org/movie/"+movie_id+"-"+titlee+"\n"+
-        "Vote avg: "+json.vote_average+"\n"+
-        "Vote count: "+json.vote_count+"\n"+
-        "Backdrops: "+json.images.backdrops+"\n"
-    );
-    if(json.videos.results.length>0){
-        for(let i=0; i<json.videos.results.length; i++){
-            console.log(json.videos.results[i].name+": https://www.youtube.com/watch?v="+json.videos.results[i].key+"\n");
-        }   
-    }
-}
+const printData = json => {
+	let title = document.createElement('h1');
+	title.textContent = 'Title: ' + json.title;
+	appendToHtml('body', title);
 
-const printFilms = (json) => {
-        /*o    (0.25 pkt) Tytuł filmu
+	if (json.poster_path != null) {
+		let poster = document.createElement('img');
+		poster.src = 'https://image.tmdb.org/t/p/w500' + json.poster_path;
+		poster.style.width = '100px';
+		poster.style.height = '100px';
+		appendToHtml('body', poster);
+	}
+
+	let overwiev = document.createElement('h2');
+	overwiev.textContent = 'Overwiev: ' + json.overview;
+	appendToHtml('body', overwiev);
+
+	let release_date = document.createElement('h2');
+	release_date.textContent = 'Release date: ' + json.release_date;
+	appendToHtml('body', release_date);
+
+	let genre = document.createElement('h2');
+	appendToHtml('body', genre);
+	if (json.genres.length > 0) {
+		for (let i = 0; i < json.genres.length; i++) {
+			genre.innerHTML += '- ' + json.genres[i].name + '<br>';
+		}
+	} else {
+		genre.innerHTML = 'IDK';
+	}
+
+	let themoviedb_link = document.createElement('a');
+	const titles = json.title.toLowerCase().split(' ');
+	const titlee = titles.join('_');
+	themoviedb_link.href = 'https://www.themoviedb.org/movie/' + movie_id + '-' + titlee;
+	themoviedb_link.textContent = 'Open on themoviedb';
+	appendToHtml('body', themoviedb_link);
+
+	let vote_average = document.createElement('h3');
+	vote_average.textContent = 'Average vote: ' + json.vote_average;
+	appendToHtml('body', vote_average);
+
+	let vote_count = document.createElement('h3');
+	vote_count.textContent = 'Count vote: ' + json.vote_count;
+	appendToHtml('body', vote_count);
+
+	if (json.images.backdrops.length > 0) {
+		let backdrops = document.createElement('div');
+		backdrops.id = 'backdrops_' + id_backdrops;
+		appendToHtml('body', backdrops);
+		for (let i = 0; i < json.images.backdrops.length; i++) {
+			let backdrop = document.createElement('img');
+			backdrop.src = 'https://image.tmdb.org/t/p/w500' + json.images.backdrops[i].file_path;
+			backdrop.alt = 'Backdrop ' + i;
+			backdrop.style.width = 200;
+			backdrop.style.height = 200;
+			appendToHtml('#backdrops_' + id_backdrops, backdrop);
+		}
+		id_backdrops++;
+	}
+
+	if (json.videos.results.length > 0) {
+		let trailers = document.createElement('div');
+		trailers.id = 'trailers_' + id_trailers;
+		appendToHtml('body', trailers);
+		for (let i = 0; i < json.videos.results.length; i++) {
+			let trailer = document.createElement('a');
+			trailer.textContent = 'Open: ' + json.videos.results[i].name;
+			trailer.href = 'https://www.youtube.com/watch?v=' + json.videos.results[i].key;
+			appendToHtml('#trailers_' + id_trailers, trailer);
+		}
+		id_trailers++;
+	}
+
+	console.log(json);
+
+	console.log(
+		'Title: ' +
+			json.title +
+			'\n' +
+			'Poster: href: https://image.tmdb.org/t/p/w500' +
+			json.poster_path +
+			'\n' +
+			'Overwiev: ' +
+			json.overview +
+			'\n' +
+			'Release date: ' +
+			json.release_date +
+			'\n' +
+			'Genre: ' +
+			json.genres +
+			'\n' +
+			'themoviedb link: https://www.themoviedb.org/movie/' +
+			movie_id +
+			'-' +
+			titlee +
+			'\n' +
+			'Vote avg: ' +
+			json.vote_average +
+			'\n' +
+			'Vote count: ' +
+			json.vote_count +
+			'\n' +
+			'Backdrops: ' +
+			json.images.backdrops +
+			'\n'
+	);
+	if (json.videos.results.length > 0) {
+		for (let i = 0; i < json.videos.results.length; i++) {
+			console.log(
+				json.videos.results[i].name + ': https://www.youtube.com/watch?v=' + json.videos.results[i].key + '\n'
+			);
+		}
+	}
+};
+
+const printFilms = json => {
+	/*o    (0.25 pkt) Tytuł filmu
         o    (0.25 pkt) Zdjęcie/plakat filmu
         o    (0.25 pkt) Opis filmu
         o    (0.25 pkt) Data premiery
@@ -83,13 +166,23 @@ const printFilms = (json) => {
         jeżeli człowiek jest reżyserem/aktorem zapisujemy jego cast.id i robimy fetcha https://api.themoviedb.org/3/person/{person_id}?append_to_response=images
         pobieramy szerokość z images.profiles.width
         */
-    console.log(json);
-    for(let i=0; i<json.results.length; i++){
-        movie_id = json.results[i].id;
-        printAll(url+'/movie/'+movie_id+'?append_to_response=videos,images,credits&include_image_language=en,null&'+api_key, printData);
-    }
-}
+	for (let i = 0; i < json.results.length; i++) {
+		movie_id = json.results[i].id;
+		printAll(
+			url +
+				'/movie/' +
+				movie_id +
+				'?append_to_response=videos,images,credits&include_image_language=en,null&' +
+				api_key,
+			printData
+		);
+	}
+};
 
-const printError = (error) => {
-    console.error('Wystąpił błąd:', error);
-}
+const printError = error => {
+	console.error('Wystąpił błąd:', error);
+};
+
+const appendToHtml = (selector, element) => {
+	document.querySelector(selector).appendChild(element);
+};
