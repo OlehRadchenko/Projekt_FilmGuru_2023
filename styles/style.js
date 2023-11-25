9a53434f6e0e4a25090814f44e48b8c6fc6bccef
Ciufciuf <PaluchBartosz@onet.eu>
2023-11-25 23:08:03 +0100

    backdropsy
    added carousel to do back drops

--- styles/style.js
+++ styles/style.js
@@ -4,10 +4,6 @@ function show(value) {
 
 let drop = document.querySelector('.dropdown');
 
-// dropdown.onclick = () => {
-// 	dropdown.classList.toggle('active');
-// };
-
 drop.addEventListener('click', () => {
 	drop.classList.toggle('active');
 });
@@ -21,3 +17,65 @@ document.addEventListener('click', e => {
 document.querySelector('.dropdown').addEventListener('click', e => {
 	e.stopPropagation();
 });
+
+// KARUSELKA
+
+const carousel = document.querySelector('.carousel');
+firstImg = carousel.querySelectorAll('img')[0];
+arrowIcons = document.querySelectorAll('.wrapper i');
+
+let isDragStart = false,
+	prevPageX,
+	prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth + 18;
+let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+
+const showHideIcons = () => {
+	let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+	if (carousel.scrollLeft == 0) {
+		arrowIcons[0].style.display = 'none';
+	} else {
+		arrowIcons[0].style.display = 'block';
+	}
+	if (carousel.scrollLeft == scrollWidth) {
+		arrowIcons[1].style.display = 'none';
+	} else {
+		arrowIcons[1].style.display = 'block';
+	}
+};
+
+arrowIcons.forEach(icon => {
+	icon.addEventListener('click', () => {
+		if (icon.id == 'left') {
+			carousel.scrollLeft -= firstImgWidth;
+		} else {
+			carousel.scrollLeft += firstImgWidth;
+		}
+		setTimeout(() => showHideIcons(), 60);
+	});
+});
+
+const dragStart = e => {
+	isDragStart = true;
+	prevPageX = e.pageX;
+	prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = e => {
+	if (!isDragStart) return;
+	e.preventDefault();
+	carousel.classList.add('dragging');
+	let = positionDiff = e.pageX - prevPageX;
+	carousel.scrollLeft = prevScrollLeft - positionDiff;
+	showHideIcons();
+};
+
+const dragStop = () => {
+	isDragStart = false;
+	carousel.classList.remove('dragging');
+};
+
+carousel.addEventListener('mousedown', dragStart);
+carousel.addEventListener('mousemove', dragging);
+carousel.addEventListener('mouseup', dragStop);
+carousel.addEventListener('mouseleave', dragStop);
